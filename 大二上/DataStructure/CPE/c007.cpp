8f4afaabad7c727f20121e4c94f148f5dadfa9e3
Li <liz79574@gmail.com>
2025-09-10 22:48:29 +0800

    Update c007.cpp

--- 大二上/DataStructure/CPE/c007.cpp
+++ 大二上/DataStructure/CPE/c007.cpp
@@ -3,5 +3,26 @@ using namespace std;
 
 int main(){
   string str;
-  
+  string ans;
+  bool first = true;
+  while(getline(cin, str)){
+    for(char c : str){
+      if(c == '"'){
+        if(first){
+          ans += "``";
+          first = false;
+        }
+        else{
+          ans += "''";
+          first = true;
+        }
+      }
+      else{
+        ans += c;
+      }
+    }
+    ans += "\n";
+  }
+   cout << ans;
+  return 0;
 }
