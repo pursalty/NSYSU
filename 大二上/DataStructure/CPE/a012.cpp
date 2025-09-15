9a62399c5d6cb6812cf8b4032bce4fc312bad176
Li <liz79574@gmail.com>
2025-09-15 19:58:46 +0800

    Create a012.cpp

---
+++ 大二上/DataStructure/CPE/a012.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long a, b;
+
+    cin >> a >> b;
+    long long result = abs(a - b);
+    cout << result << endl;
+
+    return 0;
+}
