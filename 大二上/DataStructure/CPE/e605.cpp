cbf9915ac30e0eb9c56a91edd5ebadf7baa4a682
Li <liz79574@gmail.com>
2025-09-17 21:04:21 +0800

    Create e605.cpp

---
+++ 大二上/DataStructure/CPE/e605.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a, b;
+    int cnt = 1;
+    while(cin >> a >> b){
+        if(a == 0 && b == 0) break;
+        vector<vector<char>> ch(a, vector<char>(b));
+        for(int i = 0; i < a; i ++){
+            for(int j = 0; j < b; j ++){
+                cin >> ch[i][j];
+            }
+        } 
+
+        int dx[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int dy[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+        cout << "Field #" << cnt << ":" << endl;
+        for(int i = 0; i < a; i ++){
+            for(int j = 0; j < b; j ++){
+                if(ch[i][j] == '*'){
+                    cout << '*';
+                }
+                else{
+                    int count = 0;
+                    for(int d = 0; d < 8; d ++){
+                        int ni = i + dx[d], nj = j + dy[d];
+                        if(ni >= 0 && ni < a && nj >= 0 && nj < b && ch[ni][nj] == '*') count ++;
+                    }
+                    cout << count;
+                }
+            }
+            cout << endl;
+        }
+        cnt ++;
+    }
+    return 0;
+}
