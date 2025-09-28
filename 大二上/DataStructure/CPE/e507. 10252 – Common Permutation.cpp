59a17ed33f14c37cf3906849ee18904dbafb761c
Li <liz79574@gmail.com>
2025-09-28 21:56:26 +0800

    Update and rename e507.cpp to e507. 10252 – Common Permutation.cpp

--- 大二上/DataStructure/CPE/e507. 10252 – Common Permutation.cpp
+++ 大二上/DataStructure/CPE/e507. 10252 – Common Permutation.cpp
@@ -1,26 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
     string a, b;
-    while(cin >> a >> b){
+    while(getline(cin, a) && getline(cin, b)){
         sort(a.begin(), a.end());
         sort(b.begin(), b.end());
         string str = "";
         int i = 0, j = 0;
-        while(i <= a.size() || j <= b.size()){
+        while(i < a.size() && j < b.size()){
             if(a[i] == b[j]){
                 str += a[i];
-                i ++;
-                j ++;
+                i++;
+                j++;
             }
             else if(a[i] < b[j]){
-                if(i == a.size()) break;
-                i ++;
+                i++;
             }
-            else {
-                if(j == b.size()) break;
-                j ++;
+            else{
+                j++;
             }
         }
         cout << str << endl;
